58968109cb6dcf5a4ea32381877dde076007f17a
Usuario <Usuario@DESKTOP-CU4LOL3>
2017-05-20 11:09:42 -0300

    Merge origin/master
    Conflicts:
    	src/Prueba.java

--- src/Local.java
+++ src/Local.java
@@ -48,7 +48,7 @@ public final class Local {
                   +"Dirección:"+this.getDireccion();
        }
     @Override
-       public boolean equals(Object o){
+        public boolean equals(Object o){
            Local l=(Local)o;
            return this.getNombre().equals(l.getNombre())
                    && this.getTipo()==l.getTipo();
